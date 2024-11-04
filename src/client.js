241a7f35c7b9f48c2ce6d462de6ef8c868ce4f66
omarmadjitov <orules00@icloud.com>
2024-11-04 00:25:22 -0500

    first commit

--- src/client.js
+++ src/client.js
@@ -1,9 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+const API_KEY = import.meta.env.API_KEY;
 const URL = "https://ieqtzwpbivaqaajswfkv.supabase.co";
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcXR6d3BiaXZhcWFhanN3Zmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA2NjU5ODgsImV4cCI6MjA0NjI0MTk4OH0.8sv1OvH_wJTKJtprkzNEwp5ud9Dgw8jX1Wo-36ehb50";
-
-//   VITE_SUPABASE_URL=https://ieqtzwpbivaqaajswfkv.supabase.co
-//   VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcXR6d3BiaXZhcWFhanN3Zmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA2NjU5ODgsImV4cCI6MjA0NjI0MTk4OH0.8sv1OvH_wJTKJtprkzNEwp5ud9Dgw8jX1Wo-36ehb50
 
 export const supabase = createClient(URL, API_KEY);
